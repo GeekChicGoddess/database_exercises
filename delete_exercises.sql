84dade61f111a83d74d6c822b8e62ada2cba6f3c
melodytempleton <mtempletoncantu@gmail.com>
2017-06-01 09:45:54 -0500

    second delete exercise complete

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,2 +1,5 @@
 select * from albums where release_date > 1991;
 DELETE from albums where release_date > 1991;
+SELECT * FROM albums where genre like '%disco%';
+DELETE FROM albums where genre like '%disco%';
+SELECT * FROM albums where genre like '%disco%';
