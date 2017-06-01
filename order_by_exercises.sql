972588825acdbd23051b7ae3e1c08c505e6e0a1a
melodytempleton <mtempletoncantu@gmail.com>
2017-06-01 16:32:32 -0500

    3rd order exercise complete

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,4 +1,4 @@
-SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya'OR first_name = 'Maya') and gender = 'M' ORDER BY first_name, last_name;
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya'OR first_name = 'Maya') and gender = 'M' ORDER BY last_name, first_name;
 SELECT * FROM employees WHERE last_name LIKE 'E%e';
 SELECT * FROM employees WHERE (hire_date BETWEEN CAST('1990-01-01' AS DATE) AND CAST('1999-12-31' AS DATE))
                               AND birth_date LIKE '%-12-25';
