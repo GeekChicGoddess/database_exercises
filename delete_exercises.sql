5f8f12f1540e4cdd892be12bd93bb8ac0512d870
melodytempleton <mtempletoncantu@gmail.com>
2017-06-01 09:48:13 -0500

    second delete exercise complete

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -3,3 +3,6 @@ DELETE from albums where release_date > 1991;
 SELECT * FROM albums where genre like '%disco%';
 DELETE FROM albums where genre like '%disco%';
 SELECT * FROM albums where genre like '%disco%';
+SELECT * FROM albums where artist = 'Guns N'' Roses';
+DELETE FROM albums where artist = 'Guns N'' Roses';
+SELECT * FROM albums where artist = 'Guns N'' Roses';
