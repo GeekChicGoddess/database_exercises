55ae40e8524ea94d3e530042d61640deaa11bf3c
melodytempleton <mtempletoncantu@gmail.com>
2017-06-01 15:24:01 -0500

    2nd part of additional where exercises complete

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,4 @@
-SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya'OR first_name = 'Maya';
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya'OR first_name = 'Maya') and gender = 'M';
 SELECT * FROM employees WHERE first_name LIKE 'E%';
 SELECT * FROM employees WHERE hire_date BETWEEN CAST('1990-01-01' AS DATE) AND CAST('1999-12-31' AS DATE);
 SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
