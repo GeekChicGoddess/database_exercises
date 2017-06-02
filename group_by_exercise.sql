f3531658fb48a32cbf7727740c72c876882f541e
melodytempleton <mtempletoncantu@gmail.com>
2017-06-02 09:23:03 -0500

    update last-name search to group by first_name

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,2 +1,2 @@
 SELECT DISTINCT title FROM titles ORDER BY title;
-SELECT last_name FROM employees WHERE last_name LIKE 'E%e' GROUP BY last_name ASC;
+SELECT last_name, first_name FROM employees WHERE last_name LIKE 'E%e' ORDER BY last_name GROUP BY first_name;
