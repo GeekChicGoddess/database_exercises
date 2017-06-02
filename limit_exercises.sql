621dad13f865d883f35ee7363fef6a31285d35d8
melodytempleton <mtempletoncantu@gmail.com>
2017-06-02 10:42:16 -0500

    complete last part of limit exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,3 +1,3 @@
 SELECT last_name FROM employees GROUP BY last_name DESC LIMIT 10;
-SELECT * FROM employees WHERE hire_date like '199%' AND birth_date LIKE '%-12-25' ORDER BY hire_date DESC, birth_date LIMIT 5 OFFSET 50;
+SELECT * FROM employees WHERE hire_date like '199%' AND birth_date LIKE '%-12-25' ORDER BY birth_date, hire_date DESC LIMIT 5 OFFSET 50;
 
