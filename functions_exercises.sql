b28284c1318ed79eb5bc8b0023cdda456b2dd9c1
melodytempleton <mtempletoncantu@gmail.com>
2017-06-02 11:34:52 -0500

    Complete first_name last_name concat exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,5 +1,5 @@
 SELECT count(gender), gender FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya'OR first_name = 'Maya') group by gender;
-SELECT * FROM employees WHERE last_name LIKE 'E%e' ORDER BY emp_no;
+SELECT concat(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E%e' ORDER BY emp_no;
 SELECT * FROM employees WHERE (hire_date BETWEEN CAST('1990-01-01' AS DATE) AND CAST('1999-12-31' AS DATE))
                               AND birth_date LIKE '%-12-25' ORDER BY hire_date DESC, birth_date;
 SELECT * FROM employees WHERE last_name  LIKE '%q%'AND last_name NOT LIKE '%qu%';
