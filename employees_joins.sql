d4ca40697203ce3f0103d2bc4eadfdb8d9e88f2c
melodytempleton <mtempletoncantu@gmail.com>
2017-06-02 16:35:19 -0500

    complete second employee join exercise

--- employees_joins.sql
+++ employees_joins.sql
@@ -2,5 +2,5 @@ SELECT departments.dept_name as 'Department Name', concat(employees.first_name, 
 FROM employees
 JOIN dept_manager ON employees.emp_no = dept_manager.emp_no
 JOIN departments ON dept_manager.dept_no = departments.dept_no
-WHERE to_date > now();
+WHERE to_date > now() and employees.gender = 'F';
 
