5c19df8033e441881c21bc2bcd5ddf53407d4d33
melodytempleton <mtempletoncantu@gmail.com>
2017-06-02 16:49:51 -0500

    complete third employee join exercise

--- employees_joins.sql
+++ employees_joins.sql
@@ -2,5 +2,13 @@ SELECT departments.dept_name as 'Department Name', concat(employees.first_name, 
 FROM employees
 JOIN dept_manager ON employees.emp_no = dept_manager.emp_no
 JOIN departments ON dept_manager.dept_no = departments.dept_no
-WHERE to_date > now() and employees.gender = 'F';
+WHERE dept_emp.to_date > now() and employees.gender = 'F';
 
+
+SELECT titles.title as 'Title', count(*) as Count
+FROM employees
+  JOIN titles ON employees.emp_no = titles.emp_no
+  JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+  JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE dept_emp.to_date > now() and departments.dept_no = 'd009'
+GROUP BY titles.title;
