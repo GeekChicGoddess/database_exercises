9a2d4ea77ecdd288d4075756d345ee28de32e907
melodytempleton <mtempletoncantu@gmail.com>
2017-06-02 10:36:07 -0500

    Complete limit exercise 5 xmas employess from 90's

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,1 +1,2 @@
 SELECT last_name FROM employees GROUP BY last_name DESC LIMIT 10;
+SELECT * FROM employees WHERE hire_date like '199%' AND birth_date LIKE '%-12-25' ORDER BY hire_date DESC, birth_date LIMIT 5;
