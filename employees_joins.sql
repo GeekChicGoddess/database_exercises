811058c65ed6eecf25ee0bc1586247d996a573be
melodytempleton <mtempletoncantu@gmail.com>
2017-06-02 16:34:01 -0500

    complete first employee join exercise

---
+++ employees_joins.sql
@@ -0,0 +1,6 @@
+SELECT departments.dept_name as 'Department Name', concat(employees.first_name, ' ',employees.last_name) as 'Department Manager'
+FROM employees
+JOIN dept_manager ON employees.emp_no = dept_manager.emp_no
+JOIN departments ON dept_manager.dept_no = departments.dept_no
+WHERE to_date > now();
+
