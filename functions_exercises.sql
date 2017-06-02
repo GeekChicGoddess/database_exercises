a67c109447e46543572c783e7fcbd3bbd2ecd49e
melodytempleton <mtempletoncantu@gmail.com>
2017-06-02 11:45:41 -0500

    Complete hired time exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,5 +1,5 @@
 SELECT count(gender), gender FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya'OR first_name = 'Maya') group by gender;
 SELECT concat(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E%e' ORDER BY emp_no;
-SELECT * FROM employees WHERE (hire_date BETWEEN CAST('1990-01-01' AS DATE) AND CAST('1999-12-31' AS DATE))
+SELECT emp_no, first_name, last_name, birth_date, hire_date, datediff(now(), hire_date) as EmployedTime FROM employees WHERE (hire_date BETWEEN CAST('1990-01-01' AS DATE) AND CAST('1999-12-31' AS DATE))
                               AND birth_date LIKE '%-12-25' ORDER BY hire_date DESC, birth_date;
 SELECT * FROM employees WHERE last_name  LIKE '%q%'AND last_name NOT LIKE '%qu%';
