5ee0a7c29e1f72304030fa5bba5d923b9fe35e55
melodytempleton <mtempletoncantu@gmail.com>
2017-06-05 10:48:34 -0500

    complete 4th exercise subqueries

--- sub_queries.sql
+++ sub_queries.sql
@@ -14,3 +14,8 @@ select * from employees
 join dept_manager on employees.emp_no = dept_manager.emp_no
 where dept_manager.to_date > now() and employees.gender in (select employees.gender where employees.gender = 'F');
 
+select dept_name from departments
+join dept_manager on departments.dept_no = dept_manager.dept_no
+join employees on dept_manager.emp_no = employees.emp_no
+where dept_manager.to_date > now() and employees.gender in (select employees.gender where employees.gender = 'F');
+
