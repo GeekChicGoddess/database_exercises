bd4375af3ca8932507bba65d160f1d8e879354f7
melodytempleton <mtempletoncantu@gmail.com>
2017-06-05 10:45:06 -0500

    complete 2nd and 3rd exercise subqueries

--- sub_queries.sql
+++ sub_queries.sql
@@ -3,3 +3,14 @@ use employees;
 select * from employees where hire_date in (
 select hire_date from employees where emp_no = 101010
 );
+
+select title from titles
+join employees on titles.emp_no =  employees.emp_no
+where first_name in (select first_name from employees where first_name = 'Aamod')
+group by title;
+
+
+select * from employees
+join dept_manager on employees.emp_no = dept_manager.emp_no
+where dept_manager.to_date > now() and employees.gender in (select employees.gender where employees.gender = 'F');
+
